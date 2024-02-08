7f151370f063f4438bf61fe3d7f4e99d6e6ca1d3
Evan D'Almeida <edalmei1@gmail.com>
2024-02-08 00:23:02 -0500

    jwt config

--- server/config.py
+++ server/config.py
@@ -1,35 +1,41 @@
 import os
+from dotenv import load_dotenv
 
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from flask_bcrypt import Bcrypt
+
+# used to load .env variables
+load_dotenv()
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
-app.secret_key = b'u\xd2\xdc\xe82\xa3\xc0\xca\xe7H\xd03oi\xd1\x95\xcc\x7f'
-
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'default_jwt_secret_key')
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', b'default_flask_secret_key')
 
 app.json.compact = False
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
 })
+db = SQLAlchemy(metadata=metadata, app=app)
 
-db = SQLAlchemy(metadata=metadata)
-db.init_app(app)
 migrate = Migrate(app, db)
-
 api = Api(app)
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
-
-bcrypt = Bcrypt()
-
-
+CORS(
+    app,
+    resources={r"/*": 
+               {"origins": "http://localhost:3000"}
+               }
+    )
